6862263724097e98e76b836ec6d7785336d9e36c
Eric Leong <leong@cooper.edu>
2014-11-03 21:52:30 -0500

    Use latest minicolors library.

--- js/script.js
+++ js/script.js
@@ -2,38 +2,18 @@ var texter = new ThreeDTexter($('#text_container')[0]);
 
 $(document).ready( function() {
 			
-	$('.demo').each( function() {
-		//
-		// Dear reader, it's actually very easy to initialize MiniColors. For example:
-		//
-		//  $(selector).minicolors();
-		//
-		// The way I've done it below is just for the demo, so don't get confused 
-		// by it. Also, data- attributes aren't supported at this time. Again, 
-		// they're only used for the purposes of this demo.
-		//
+	$('#colors input').each( function() {
 		$(this).minicolors({
-			control: $(this).attr('data-control') || 'hue',
-			defaultValue: $(this).attr('data-defaultValue') || '',
-			inline: $(this).attr('data-inline') === 'true',
-			letterCase: $(this).attr('data-letterCase') || 'lowercase',
-			opacity: $(this).attr('data-opacity'),
-			position: $(this).attr('data-position') || 'bottom left',
 			change: function(hex, opacity) {
-				var log;
-				var div_element = $(this).parents('.form-group');
 				try {
-					log = hex ? hex : 'transparent';
-					if( opacity ) log += ', ' + opacity;
-					div_element.css('background-color', log);
-
 					texter.api.setColor(
 						parseInt($('#primary-color').val().substr(1), 16), 
 						parseInt($('#secondary-color').val().substr(1), 16));
-				} catch(e) {}
-			},
-			theme: 'default'
-		});            
+				} catch(e) {
+					console.log(e);
+				}
+			}
+		});
 	});
 
 	var rendering = false;
